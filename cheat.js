27b369ca61eac61e8f15f64e3880e5a5b710536c
elliot_ <112902069+akatiggerx04@users.noreply.github.com>
2024-11-03 15:44:54 +0300

    Add files via upload

---
+++ cheat.js
@@ -0,0 +1,237 @@
+let gameLocations = [];
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function isInNormalMode() {
+  var infoDiv = document.querySelector("div.info");
+  if (infoDiv) {
+    var totalDiv = infoDiv.querySelector("div.total");
+    if (totalDiv) {
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    return false;
+  }
+}
+
+function clickMapCenter() {
+  const panelElement = document.querySelector('[data-control="Panel"]');
+  const panelRect = panelElement.getBoundingClientRect();
+  const screenWidth = window.innerWidth;
+  const remainingWidth = screenWidth - panelRect.width;
+  const x = panelRect.width + remainingWidth / 2;
+  const y = window.innerHeight / 2 - 13;
+  const clickEvent = new MouseEvent("click", {
+    bubbles: true,
+    cancelable: true,
+    view: window,
+    detail: 1,
+    clientX: x,
+    clientY: y,
+    ctrlKey: false,
+    altKey: false,
+    shiftKey: false,
+    metaKey: false,
+    button: 0,
+    buttons: 1,
+    relatedTarget: null,
+    screenX: x,
+    screenY: y,
+    pageX: x,
+    pageY: y,
+    movementX: 0,
+    movementY: 0,
+    offsetDirection: 0,
+    layerX: x,
+    layerY: y,
+    fromElement: null,
+    toElement: document.elementFromPoint(x, y),
+    currentTarget: document.elementFromPoint(x, y),
+    target: document.elementFromPoint(x, y),
+    isTrusted: true,
+  });
+  document.elementFromPoint(x, y).dispatchEvent(clickEvent);
+}
+
+async function getVersusLocations() {
+  const lobbyID = localStorage.getItem("lobbyId");
+  const lobbyUserID = localStorage.getItem("lobbyUserId");
+
+  let response = await fetch("https://gtaguessr.com/API/GetVersusLocations", {
+    method: "POST",
+    body: JSON.stringify({
+      lobbyId: lobbyID,
+      lobyUserId: lobbyUserID,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  response = await response.json();
+  gameLocations = response?.locations ?? [];
+}
+
+async function getLocationByID(id) {
+  selectedRound = id + 1;
+  const lobbyID = localStorage.getItem("lobbyId");
+  const lobbyUserID = localStorage.getItem("lobbyUserId");
+
+  let response = await fetch("https://gtaguessr.com/API/SubmitAGuess", {
+    method: "POST",
+    body: JSON.stringify({
+      sessionId: getRandomInt(10000000, 99999999).toString(),
+      locationId: gameLocations[id]?.locationId ?? 0,
+      lat: `${getRandomInt(-7000, 7000)}.${getRandomInt(0, 99999)}`,
+      lng: `${getRandomInt(7000, 7000)}.${getRandomInt(0, 99999)}`,
+      lobyId: lobbyID,
+      lobyUserId: lobbyUserID + "5",
+      game: "1",
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  response = await response.json();
+  const lat = response?.lat ?? 0;
+  const lng = response?.lng ?? 0;
+
+  return { lat, lng };
+}
+
+async function setLocation(locationID) {
+  changeCopyrightText("Finding the exact coordinates...");
+  let exactLocation;
+  try {
+    exactLocation = await getLocationByID(locationID);
+  } catch {
+    alert("Error while finding exact location");
+    return;
+  }
+
+  window.location.href =
+    "/Guess#5/" + exactLocation.lat + "/" + exactLocation.lng;
+
+  changeCopyrightText("Setting map marker...");
+
+  await new Promise((resolve) => {
+    setTimeout(resolve, 2000);
+  });
+  clickMapCenter();
+
+  changeCopyrightText(
+    "Select the round number to locate the coordinates for it",
+  );
+}
+
+// UI
+
+function changeCopyrightText(text) {
+  const element = document.getElementById("copyright");
+  element.innerText = "© GtaGuessr | " + text;
+}
+
+async function getLocationsAndInitUI() {
+  if (document.getElementById("cheatLocations")) {
+    document.getElementById("cheatLocations").remove();
+    changeCopyrightText("Reloading Locations...");
+  } else {
+    changeCopyrightText("Finding Locations...");
+  }
+
+  try {
+    await getVersusLocations();
+  } catch (e) {
+    alert("Error while getting locations!");
+    console.error(e.message);
+    return;
+  }
+
+  changeCopyrightText(
+    "Select the round number to locate the coordinates for it",
+  );
+
+  document.getElementById("cheatFindLocations").remove();
+
+  const targetElement = document.querySelector('[data-control="Panel"]');
+
+  const newElement = document.createElement("div");
+  newElement.id = "cheatLocations";
+  newElement.style.display = "grid";
+  newElement.style.gridTemplateColumns = "repeat(5, 1fr)";
+  newElement.style.columnGap = "5px";
+
+  const buttons = [
+    { id: "setFirstLocation", onclick: "setLocation(0);", text: "1" },
+    { id: "setSecondLocation", onclick: "setLocation(1);", text: "2" },
+    { id: "setThirdLocation", onclick: "setLocation(2);", text: "3" },
+    { id: "setFourthLocation", onclick: "setLocation(3);", text: "4" },
+    { id: "setFifthLocation", onclick: "setLocation(4);", text: "5" },
+  ];
+
+  buttons.forEach((button) => {
+    const buttonElement = document.createElement("button");
+    buttonElement.id = button.id;
+    buttonElement.style.backgroundColor = "#f83849";
+    buttonElement.style.border = "none";
+    buttonElement.style.padding = "10px 10px";
+    buttonElement.style.width = "100%";
+    buttonElement.style.fontWeight = "500";
+    buttonElement.style.color = "white";
+    buttonElement.onclick = new Function(button.onclick);
+    buttonElement.textContent = button.text;
+    newElement.appendChild(buttonElement);
+  });
+
+  targetElement.insertBefore(newElement, targetElement.firstChild);
+}
+
+async function activateCheats() {
+  if (window.location.host != "gtaguessr.com") {
+    alert(
+      "Unsupported website! Cheats only work on gtaguessr.com (Versus Mode)!",
+    );
+    return;
+  }
+
+  if (isInNormalMode()) {
+    alert("Normal Mode is not supported! Only Versus Mode is!");
+    return;
+  }
+
+  if (document.body.attributes["cheats-init"]) {
+    alert("Cheats already activated!");
+    return;
+  } else {
+    document.body.attributes["cheats-init"] = "yes";
+  }
+
+  const findLocationsCheatElement = document.createElement("button");
+
+  findLocationsCheatElement.style.border = "none";
+  findLocationsCheatElement.id = "cheatFindLocations";
+  findLocationsCheatElement.style.backgroundColor = "#f83849";
+  findLocationsCheatElement.style.padding = "10px 50px";
+  findLocationsCheatElement.style.fontWeight = "500";
+  findLocationsCheatElement.style.color = "white";
+  findLocationsCheatElement.style.zIndex = "9999999999";
+  findLocationsCheatElement.style.bottom = "50px";
+  findLocationsCheatElement.style.position = "absolute";
+  findLocationsCheatElement.style.right = "20px";
+  findLocationsCheatElement.onclick = getLocationsAndInitUI;
+  findLocationsCheatElement.textContent = "Find Locations";
+
+  document.body.insertBefore(
+    findLocationsCheatElement,
+    document.body.firstChild,
+  );
+
+  changeCopyrightText("Versus Mode Cheats Activated");
+}
+
+activateCheats();
